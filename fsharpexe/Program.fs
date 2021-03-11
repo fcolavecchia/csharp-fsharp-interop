89771da9a9e41bab1fe4917f9dd592386356e16f
Flavio Colavecchia <fcolavecchia@gmail.com>
2021-03-11 12:28:33 -0300

    Done with comments in F#

--- fsharpexe/Program.fs
+++ fsharpexe/Program.fs
@@ -5,6 +5,9 @@ open Csharpdll
 open Fsharpdll.Visa
 open Fsharpdll.Person
 
+// A 'type cast', or simply, a name change
+// from the C# class to the F# record
+type PersonF = PersonC
 
 [<EntryPoint>]
 let main argv =
@@ -25,22 +28,64 @@ let main argv =
 
     
     printfn "\nConsuming C#"      
+    // Create a record type from a C# Class constructor
     let touristC = PersonC("John C. Doe", Status.Tourist)
     let studentC = PersonC("Mary C. Eod", Status.Student)
 
+    // Use the method from a C# Class that returns a Result type
+    let resultOkC = PersonC.ValidateStudentC studentC
+
+    // The method returns Ok PersonC 
+    match resultOkC with 
+    | Ok s -> printfn $"{s.Name} is a student" 
+    | Error e -> printfn $"{studentC.Name} is a {e}" 
+
+
+    let resultErrorC = PersonC.ValidateStudentC touristC
+    
+    // or the method returns Error string
+    match resultErrorC with 
+    | Ok s -> printfn $"{s.Name} is a student" 
+    | Error e -> printfn $"{touristC.Name} is a {e}"
+
+
+    printfn "\nConsuming C#, overloaded constructor from F# type"
+    // Here we use the overloaded constructor in the C# Class
+    // that receives a Person.Person F# record to build an
+    // equivalent object and comes back as a PersonC record
+    // 
     let anotherTouristC = PersonC tourist
     let anotherStudentC = PersonC student
 
-    printfn "result = %A" (PersonC.ValidateStudentC touristC)
-    printfn "result = %A" (PersonC.ValidateStudentC studentC)
+    let resultOkF = PersonF.ValidateStudentC anotherStudentC
+    match resultOkF with 
+    | Ok s -> printfn $"{s.Name} is a student" 
+    | Error e -> printfn $"{anotherStudentC.Name} is a {e}" 
 
-    printfn "Consuming C#, casting de F#"      
-    let anotherTouristC = PersonC tourist
-    let anotherStudentC = PersonC student
+    let resultErrorF = PersonF.ValidateStudentC anotherTouristC
+    match resultErrorF with 
+    | Ok s -> printfn $"{s.Name} is a student" 
+    | Error e -> printfn $"{anotherTouristC.Name} is a {e}"
 
-    printfn "result = %A" (PersonC.ValidateStudentC anotherTouristC)
-    printfn "result = %A" (PersonC.ValidateStudentC anotherStudentC)
+    printfn "\nConsuming C#, casting  F#"
+    // This is maximal obfuscation!
+    // Here we use the overloaded constructor in the C# Class
+    // that receives a Person.Person F# record to build an
+    // equivalent object and comes back as a PersonC,
+    // but we use the type 'cast' in line 8...
+    // That cast is simply a name change, just in case
+    // one needs it.
+    let yetAnotherTouristF = PersonF tourist
+    let yetAnotherStudentF = PersonF student
 
+    let resultOkF = PersonF.ValidateStudentC yetAnotherStudentF
+    match resultOkF with 
+    | Ok s -> printfn $"{s.Name} is a student" 
+    | Error e -> printfn $"{yetAnotherStudentF.Name} is a {e}" 
 
+    let resultErrorF = PersonF.ValidateStudentC yetAnotherTouristF
+    match resultErrorF with 
+    | Ok s -> printfn $"{s.Name} is a student" 
+    | Error e -> printfn $"{yetAnotherTouristF.Name} is a {e}"
 
     0 // return an integer exit code
